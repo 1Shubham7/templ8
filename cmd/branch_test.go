0ba351270b8eb724aacfcbd1a3afa8b0cf917910
Shubham Singh <shubhammahar1306@gmail.com>
2024-12-22 12:01:55 +0530

    UTs for branch.go
    Signed-off-by: Shubham Singh <shubhammahar1306@gmail.com>

--- cmd/branch_test.go
+++ cmd/branch_test.go
@@ -5,41 +5,31 @@ import (
 	"testing"
 )
 
-// Test GetDefaultBranch function by making a real API call
 func TestGetDefaultBranch(t *testing.T) {
 	assert := assert.New(t)
 
-	// Replace these with actual valid GitHub repository and username
 	username := "1shubham7"
 	repo := "templ8"
 
-	// Call GetDefaultBranch function
 	defaultBranch, err := GetDefaultBranch(username, repo)
 
-	// Assertions
 	assert.Nil(err)
-	assert.Equal("main", defaultBranch, "The default branch should be 'main'")
+	assert.Equal("main", defaultBranch)
 }
 
-// Test VerifyBranchName function by making a real API call
 func TestVerifyBranchName(t *testing.T) {
 	assert := assert.New(t)
 
-	// Replace these with actual valid GitHub repository and username
 	username := "1shubham7"
 	repo := "templ8"
 
-	// Test case where the branch exists
 	branch := "main"
 	branchExists := VerifyBranchName(username, repo, branch)
 
-	// Assertions
-	assert.True(branchExists, "Branch 'main' should exist")
+	assert.True(branchExists)
 
-	// Test case where the branch does not exist
 	branch = "nonexistent"
 	branchExists = VerifyBranchName(username, repo, branch)
 
-	// Assertions
-	assert.False(branchExists, "Branch 'nonexistent' should not exist")
+	assert.False(branchExists)
 }
